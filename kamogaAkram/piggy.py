d5edd37d203054c3247057206e8b9ca16d8d1980
Akram <akramkamoga17@gmail.com>
2025-10-07 15:12:04 +0300

    new

---
+++ kamogaAkram/piggy.py
@@ -0,0 +1,32 @@
+coins = 100
+
+
+coins = 100 + 500
+print('i have:', coins,'coins')
+
+coins  = -999
+
+
+class PiggyBank:
+    def __init__(self,coins):
+        self._coins = coins
+        self.put_in(coins)
+
+    def put_in(self,amount):
+        if amount <=0:
+            raise ValueError('cant add 0 money')
+        self._coins +=amount
+
+    def take_out(self,amount):
+        if amount <= 0:
+            raise ValueError('cant take out 0 money')
+        if amount > self.coins:
+            raise ValueError('not enough money')
+        self._coins -= amount    
+
+    def how_much(self):
+        return self._coins        
+
+akram = PiggyBank(100)
+akram.put_in(300)
+print("akram's box has:",akram.how_much(), 'coins')
---
+++ kamogaAkram/rltp.py
@@ -0,0 +1,9 @@
+class RTracker:
+    def __init__(self,partner):
+        self.partner= partner
+        self._mood= 'happy'
+        self.__trust =50
+        self._vibe = 
+
+
+    
