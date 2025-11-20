1dce3561b1537545b269b648c142380458ce9ddf
Akram <akramkamoga17@gmail.com>
2025-11-20 09:10:25 +0300

    new

---
+++ kamogaAkram/lists.py
@@ -0,0 +1,23 @@
+gadgets=['laptop','ipad','phone','mp3player','smartwatch']
+gadgets.append('drone')
+gadgets.remove('laptop')
+print('after append',gadgets)
+print('after removing',gadgets)
+
+
+
+veichle= ('toyota','supramk4')
+print('veichle',veichle)
+
+
+
+
+#best car models
+
+CarBrand = {
+    'porsche': 911,
+    'nissan': 35,
+    'mazda':311,
+}
+
+print ('CarBrand',CarBrand)
